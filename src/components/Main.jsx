fc361fa8316af09a7d14b66d9ea53bed27e59c14
Proservices Ingenieria <reydavidquiroz@gmail.com>
2024-01-17 22:36:09 -0500

    edit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ import Gallery from "./Gallery";
 const Main = () => {
   const { themma } = useAuth();
   return (
-    <div className={` ${themma}`}>
+    <div className=  {`h-screen  ${themma}`}>
       <Gallery />
     </div>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,10 +5,9 @@ import Main from "../components/Main";
 const HomePage = () => {
   return (
     <div>
-       <Header />
+      <Header />
       <Main />
-       <Footer /> 
-      
+      <Footer />
     </div>
   );
 };
